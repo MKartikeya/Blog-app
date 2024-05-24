d09d139edfb601a24d68725384754cb5d3da2a47
MKartikeya <kartikeya.repo@gmail.com>
2024-05-24 23:10:04 +0530

    origin update

--- backend/index.js
+++ backend/index.js
@@ -23,7 +23,7 @@ const connectToDB=async()=>{
 
 dotenv.config();
 app.use(express.json());
-app.use(cors({origin:"http://localhost:5173",credentials:true}))
+app.use(cors({origin:"https://deploy-test-gamma-nine.vercel.app/",credentials:true}))
 app.use(cookieParser());
 app.use("/api/auth",authRounter);
 app.use("/api/users",userRouter);
