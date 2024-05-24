af72ce29605b8518e1b1cd9aaa1157c9bf4c6fb1
MKartikeya <kartikeya.repo@gmail.com>
2024-05-25 00:22:44 +0530

    origin update

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -27,7 +27,7 @@ router.post("/register",async(req,res)=>{
 
 
 //Login
-router.post("/login/",async(req,res)=>{
+router.post("/login",async(req,res)=>{
     try{
         const user=await User.findOne({email:req.body.email});
         if(!user){
