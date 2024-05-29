611b3bd6b756456ac3106b28c5480f258a78f94f
MKartikeya <kartikeya.repo@gmail.com>
2024-05-29 12:46:38 +0530

    origin update

--- frontend/src/components/Comment.jsx
+++ frontend/src/components/Comment.jsx
@@ -24,9 +24,9 @@ const Comment = ({ c,post }) => {
                 if (result.isConfirmed) {
             const res = await axios.delete(URL + "/api/comments/" + comment._id, { withCredentials: true });
             Swal.fire("Comment Deleted!", "", "success");
-            // navigate("/posts/post/"+post._id)
+            navigate("/posts/post/"+post._id)
 
-            window.location.reload();
+            // window.location.reload();
             // navigate("/posts/post/"+post._id)
                 } else if (result.isDenied) {
                     Swal.fire("Delete Cancelled", "", "info");
@@ -61,8 +61,8 @@ const Comment = ({ c,post }) => {
                 if (result.isConfirmed) {
                     try{
                         const res = await axios.put(URL + "/api/comments/"+comment._id, { comment: newcom, postId: post._id, author: user.username, userId: user._id }, { withCredentials: true })
-                        window.location.reload();
-                        // navigate("/posts/post/"+post._id);
+                        // window.location.reload();
+                        navigate("/posts/post/"+post._id);
                         Swal.fire("Updated!", "", "success");
                     } catch(err){
                         Swal.fire("Some error occurred", "", "error");
