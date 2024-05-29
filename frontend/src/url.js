7c2ea89a7976487b8132a1e21111c5ef1d632b30
MKartikeya <kartikeya.repo@gmail.com>
2024-05-29 12:42:44 +0530

    origin update

--- frontend/src/url.js
+++ frontend/src/url.js
@@ -1,5 +1,5 @@
-export const URL="http://localhost:5000"
-export const IF=import.meta.env.VITE_IF
-
-// export const URL="https://deploy-test-1-xh70.onrender.com"
+// export const URL="http://localhost:5000"
 // export const IF=import.meta.env.VITE_IF
+
+export const URL="https://deploy-test-1-xh70.onrender.com"
+export const IF=import.meta.env.VITE_IF
